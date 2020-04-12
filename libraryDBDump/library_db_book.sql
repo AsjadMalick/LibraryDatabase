067466a4e7fbc35dfeb617a57cb8ba021abe4dbd
JBenedictM <biller010@gmail.com>
2020-04-11 20:39:36 -0600

    Changed location to branch_name in book and disc table

--- libraryDBDump/library_db_book.sql
+++ libraryDBDump/library_db_book.sql
@@ -25,9 +25,11 @@ DROP TABLE IF EXISTS `book`;
 CREATE TABLE `book` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
-  `location` varchar(255) NOT NULL DEFAULT 'unknown',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `branch_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `branch_name` (`branch_name`),
+  CONSTRAINT `book_ibfk_1` FOREIGN KEY (`branch_name`) REFERENCES `branch` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -36,7 +38,7 @@ CREATE TABLE `book` (
 
 LOCK TABLES `book` WRITE;
 /*!40000 ALTER TABLE `book` DISABLE KEYS */;
-INSERT INTO `book` VALUES (1,'The Great Gatsby','unknown'),(2,'The Grapes of Wrath','unknown'),(3,'1984','unknown'),(4,'Ulysses','unknown'),(12,'Live Love Laugh','unknown');
+INSERT INTO `book` VALUES (1,'Wolf of Wall Street','central library'),(2,'The Grapes of Wrath','central library'),(3,'1984','central library'),(4,'Ulysses','central library'),(12,'Live Love Laugh',NULL),(14,'post Man Chronicles',NULL),(15,'post Man Chronicles2',NULL);
 /*!40000 ALTER TABLE `book` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -49,4 +51,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-04-06 23:52:19
+-- Dump completed on 2020-04-11 20:36:22
